0238b589aa4a33233c06ba26d8339fd75109f5af
sTeeLM <steel.mental@gmail.com>
2019-04-08 17:40:51 +0800

    fix bug

--- tc.py
+++ tc.py
@@ -22,11 +22,14 @@ data = tp.parse_torrent_file(sys.argv[1])
 
 data['info']['name'] = md5_string(data['info']['name'].encode('utf-8'))
 data['info']['publisher'] = md5_string(data['info']['publisher'].encode('utf-8'))
-data['info']['name.utf-8'] = md5_string(data['info']['name.utf-8'].encode('utf-8'))
-data['info']['publisher.utf-8'] = md5_string(data['info']['publisher.utf-8'].encode('utf-8'))
+if 'name.utf-8' in data['info']:
+	data['info']['name.utf-8'] = md5_string(data['info']['name.utf-8'].encode('utf-8'))
+if 'publisher.utf-8' in data['info']:
+	data['info']['publisher.utf-8'] = md5_string(data['info']['publisher.utf-8'].encode('utf-8'))
 
 for i in range(0, len(data['info']['files'])):
 	data['info']['files'][i]['path'][0] = md5_string(data['info']['files'][i]['path'][0].encode('utf-8'))
-	data['info']['files'][i]['path.utf-8'][0] = md5_string(data['info']['files'][i]['path.utf-8'][0].encode('utf-8'))
+	if 'path.utf-8' in data['info']['files'][i]:
+		data['info']['files'][i]['path.utf-8'][0] = md5_string(data['info']['files'][i]['path.utf-8'][0].encode('utf-8'))
 
 tp.create_torrent_file(sys.argv[2], data)
